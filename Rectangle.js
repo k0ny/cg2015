4dbf68cb05b5f7f287c0c78fdf5790b177a0a826
k0ny <vincenzo.giordano.kony@gmail.com>
2015-03-13 14:00:21 +0100

    aa

--- Rectangle.js
+++
@@ -1,8 +0,0 @@
-function Rectangle(base, altezza){
-	this.base= base;
-	this.altezza= altezza;
-}
-Rectangle.prototype.name = "rectangle";
-Rectangle.prototype.area= function() {
-	return (this.base * this.altezza);
-}
